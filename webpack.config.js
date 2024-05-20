43a06ed809641e964a232b5fb05014a9af33cb20
Sergey Krashevich <svk@svk.su>
2024-05-21 01:38:54 +0300

    feat(project): update dependencies and repository details
    - Updated project dependencies to latest versions.
    - Changed repository URL and author information in package.json.
    - Added webpack output configuration and source map support.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+import path from 'path';
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   entry: {
     'monaco-editor': './src/editor.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -31,4 +40,20 @@ export default {
       ],
     }),
   ],
+  optimization: {
+    minimize: false,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        monaco: {
+          name: 'monaco-editor',
+          test: /[\\/]node_modules[\\/](monaco-editor)[\\/]/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  devtool: 'source-map', // Enable source maps
 };
